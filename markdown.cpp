bcf5cc230d38f5d07f6e9902341eb75c65c7ecf6
xuedingebuyangmao <99530513+xuedingebuyangmao@users.noreply.github.com>
2024-12-03 21:45:19 +0800

    Add files via upload

--- markdown.cpp
+++ markdown.cpp
@@ -17,9 +17,9 @@ printf("x=%d,n=%d\n",x,n);
 printf("%d",n);
 */
 
-//(猜数游戏）
-///*
 
+//(猜数游戏）
+/*
 srand(time(0));
 int number =rand()%100+1;
 int count =0;
@@ -29,14 +29,72 @@ do{
 	scanf("%d",&a);
 	count ++;
 	if(a>number){
-		printf("bigger");
+		printf("bigger\n");
 	} else if(a<number){
-		printf("smaller");
+		printf("smaller\n");
 	}
-	}
+  }
 	while (a!=number);
 	printf("right answer,and u cost%dtimes.\n",count);
+*/
+
+
+//(求平均值） 
+/*
+int sum=0;
+int number;
+int count=0;
+
+scanf("%d", &number);
+
+while(number != -1){
+	sum += number;
+	count ++;
+	scanf("%d", &number);
 }
+printf("%f\n",1.0*sum/count);
+*/
+
+
+//(整数求逆） 
+/*
+int x;
+scanf("%d",&x);
+int digit;
+int ret=0;
+
+while(x>0){
+	digit=x%10;
+	ret=ret*10+digit;
+	printf("x=%d,digit=%d,ret=%d\n",x,digit,ret);
+	x /= 10;
+}
+printf("%d",ret);
+*/
+
+
+//(求阶乘)
+/*
+int n;
+scanf("%d", &n);
+int fact=1;
+int i=1;
+for(i=1; //初始条件(在特殊情况下可以省掉初始条件)
+i<=n;    //循环继续的条件
+i++     //循环每轮要做的条件
+)
+{
+	fact *= i;
+}
+printf("%d!=%d\n",n,fact);
+*/
+
+
+
+
+
+
+
 return 0;
 }
 
